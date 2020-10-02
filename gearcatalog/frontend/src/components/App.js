4832d4f1d7def9504689f7abf9b4626f57653644
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-10-02 11:16:48 -0700

    add Alerts component and import into App component

--- gearcatalog/frontend/src/components/App.js
+++ gearcatalog/frontend/src/components/App.js
@@ -3,20 +3,34 @@ import ReactDOM from "react-dom";
 
 import Header from "./layout/Header";
 import Dashboard from "./gear/Dashboard";
+import Alerts from "./layout/Alerts";
 
 import { Provider } from "react-redux";
 import store from "../store";
+
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+// Alert Options
+const alertOptions = {
+  timeout: 3000,
+  position: "top center",
+  transition: "scale",
+};
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Fragment>
-          <Header />
-          <div className="container">
-            <Dashboard />
-          </div>
-        </Fragment>
+        <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Fragment>
+            <Header />
+            <Alerts />
+            <div className="container">
+              <Dashboard />
+            </div>
+          </Fragment>
+        </AlertProvider>
       </Provider>
     );
   }
--- gearcatalog/frontend/src/components/gear/Dashboard.js
+++ gearcatalog/frontend/src/components/gear/Dashboard.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react'
-import Form from './Form'
-import GearList from './GearList'
+import React, { Fragment } from "react";
+import Form from "./Form";
+import GearList from "./GearList";
 
 export default function Dashboard() {
   return (
     <Fragment>
-      <Form />
       <GearList />
+      <Form />
     </Fragment>
-  )
+  );
 }
