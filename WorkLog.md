bd7fbf4a0f29a7af301c7f67a1e5a4dbbddecd88
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-10-02 16:03:31 -0700

    add knox auth token migrations to python backend

--- WorkLog.md
+++ WorkLog.md
@@ -1,5 +1,10 @@
 9/25/2020
-* 8am - 9am: work on readme
-* 9am - 11am: component diagram
-* 11am - 12pm: research on component layouts
-* 1pm - 5pm: coding
+
+- 8am - 9am: work on readme
+- 9am - 11am: component diagram
+- 11am - 12pm: research on component layouts
+- 1pm - 5pm: coding
+
+10/2/2020
+
+- 8am - 5pm: coding
--- gearcatalog/gearcatalog/settings.py
+++ gearcatalog/gearcatalog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # dotenv
@@ -23,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/'
 
-import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
@@ -45,8 +45,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'gear',
     'rest_framework',
-    'frontend'
+    'frontend',
+    'knox'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication')
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
