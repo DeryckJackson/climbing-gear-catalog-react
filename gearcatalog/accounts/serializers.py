4208fc1d5bb74b6929121721fe0bfba904b588ee
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-10-04 12:12:06 -0700

    add register and login API views to backend

--- gearcatalog/accounts/serializers.py
+++ gearcatalog/accounts/serializers.py
@@ -9,9 +9,8 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'email')
 
+
 # Register Serializer
-
-
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -25,3 +24,14 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 # Login Serializer
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
