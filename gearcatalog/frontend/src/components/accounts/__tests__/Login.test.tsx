8a10c73c205967b32a0b44b84365962457838b64
DeryckJackson <dgjackson01@gmail.com>
2020-11-30 14:38:36 -0800

    remove unneeded import statements

--- gearcatalog/frontend/src/components/accounts/__tests__/Login.test.tsx
+++ gearcatalog/frontend/src/components/accounts/__tests__/Login.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import Login from '../Login';
 import { Redirect } from 'react-router-dom';
