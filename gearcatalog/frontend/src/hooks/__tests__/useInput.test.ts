fa39fc0eaee640f094b1490268fd5479e62e76e4
DeryckJackson <dgjackson01@gmail.com>
2020-12-15 08:50:23 -0800

    add test for useInput hook

--- gearcatalog/frontend/src/hooks/__tests__/useInput.test.ts
+++ gearcatalog/frontend/src/hooks/__tests__/useInput.test.ts
@@ -2,15 +2,21 @@ import { useInput } from '../useInput';
 import React from 'react';
 
 describe('useInput', () => {
+  const setState = jest.fn();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const useStateMock: any = (initState: any) => [initState, setState];
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('Should call useInput and return correct values', () => {
-    const mock = jest.spyOn(React, 'useState')
-    .mockImplementation(jest.fn());
+    jest.spyOn(React, 'useState').mockImplementation(useStateMock);
     const { value, setValue, reset, bind } = useInput('foobar');
 
     expect(value).toBe('foobar');
-    setValue('notFoobar');
-    expect(value).toBe('notFoobar');
-    expect(reset()).toBe('foobar');
+    setValue('notfoobar');
+    expect(setState).toHaveBeenCalledTimes(1);
     expect(bind.value).toBe('foobar');
     bind.onChange({
       preventDefault: () => 1 + 1,
@@ -19,5 +25,8 @@ describe('useInput', () => {
         value: 'eventValue',
       },
     });
+    expect(setState).toHaveBeenCalledTimes(2);
+    reset();
+    expect(setState).toHaveBeenCalledTimes(3);
   });
 });
